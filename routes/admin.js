71404250e9527b97bf1cda18a6f154a777e9864e
AyasSharqawe <92643395+AyasSharqawe@users.noreply.github.com>
2022-04-23 13:10:01 -0700

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -6,44 +6,43 @@ const router = express.Router();
 
 
 const db = sql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'housings'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'housings'
 });
 
 db.connect((error) => {
-  if (error) {
-       console.log(error)
-   } else {
-    dbDebugger("MYSQL connected ...")
-  }
- });
+    if (error) {
+        console.log(error)
+    } else {
+        dbDebugger("MYSQL connected ...")
+    }
+});
 
- const upload = multer({storage:multer.memoryStorage()});
+const upload = multer({ storage: multer.memoryStorage() });
 
- router.post('/apartmentDB',upload.single('apatrmentImage'),(req,res)=>{
-   const cost = req.body.monthlyCost;
-   const space = req.body.space;
-   const description = req.body.description;
-   const city = req.body.city;
-   const location = req.body.location;
-   const roomCount = req.body.roomCount;
-   const ownerName = req.body.ownerName;
-   const ownerPhone = req.body.ownerPhone;
-   const roommates = req.body.roommates;
-   const remaining = req.body.Remaining;
-   const image = req.file.buffer.toString('base64');
-   db.query('INSERT INTO apartment SET ?' , {monthlyCost:cost , space :space , description:description , city :city , location :location , roomCount:roomCount , ownerName:ownerName , ownerPhone:ownerPhone , imageHere:image , roommates:roommates , remainingRoommates:remaining } , (error,results)=>{
-     if(error){
-       console.log(error);
-     } 
-     else {
-      return  res.redirect('/admin');
-     }
-   
- })
- })  
+router.post('/apartmentDB', upload.single('apatrmentImage'), (req, res) => {
+    const cost = req.body.monthlyCost;
+    const space = req.body.space;
+    const description = req.body.description;
+    const city = req.body.city;
+    const location = req.body.location;
+    const roomCount = req.body.roomCount;
+    const ownerName = req.body.ownerName;
+    const ownerPhone = req.body.ownerPhone;
+    const roommates = req.body.roommates;
+    const remaining = req.body.Remaining;
+    const image = req.file.buffer.toString('base64');
+    db.query('INSERT INTO apartment SET ?', { monthlyCost: cost, space: space, description: description, city: city, location: location, roomCount: roomCount, ownerName: ownerName, ownerPhone: ownerPhone, imageHere: image, roommates: roommates, remainingRoommates: remaining }, (error, results) => {
+        if (error) {
+            console.log(error);
+        } else {
+            return res.redirect('/admin');
+        }
+
+    })
+})
 
 
-      module.exports = router;
+module.exports = router;
