4d3e244d6d0dec9d195a7fc38dc83877c727c7e3
ayaabusoud <ayaabusoud23@gmail.com>
2022-04-27 15:18:06 +0300

    edit apartment info

--- routes/admin.js
+++ routes/admin.js
@@ -23,56 +23,25 @@ db.connect((error) => {
 
  const upload = multer({storage:multer.memoryStorage()});
 
- router.post('/apartmentDB',upload.single('apatrmentImage'),(req,res)=>{
-   const cost = req.body.monthlyCost;
-   const space = req.body.space;
-   const description = req.body.description;
-   const city = req.body.city;
-   const location = req.body.location;
-   const roomCount = req.body.roomCount;
-   const ownerName = req.body.ownerName;
-   const ownerPhone = req.body.ownerPhone;
-   const roommates = req.body.roommates;
-   const remaining = req.body. Remaining;
+ router.post('/apartmentDB/:id',upload.single('apatrmentImage'),(req,res)=>{
+   const id = req.params.id;
    const image = req.file.buffer.toString('base64');
-   db.query('INSERT INTO apartment SET ?' , {monthlyCost:cost , space :space , description:description , city :city , location :location , roomCount:roomCount , ownerName:ownerName , ownerPhone:ownerPhone , imageHere:image , roommates:roommates , remainingRoommates:remaining } , (error,results)=>{
+   db.query('INSERT INTO apartment SET ?' , {monthlyCost:req.body.monthlyCost , space :req.body.space , description:req.body.description , city :req.body.city , location :req.body.location , roomCount:req.body.roomCount , ownerName:req.body.ownerName , ownerPhone:req.body.ownerPhone , imageHere:image , roommates:req.body.roommates , remainingRoommates:req.body.Remaining } , (error,results)=>{
      if(error){
        console.log(error);
      } 
      else {
-      return  res.redirect('/admin');
+      db.query('SELECT * FROM user WHERE id = ?' ,[id], (error , results ) =>{
+        if(error)console.log(error)
+        else{
+            res.render('admin',{results});
+        }
+    }) 
      }
    
  })
  })  
 
-
-      module.exports = router;
-
-
-
-
-router.post('/apartmentDB', upload.single('apatrmentImage'), (req, res) => {
-    const cost = req.body.monthlyCost;
-    const space = req.body.space;
-    const description = req.body.description;
-    const city = req.body.city;
-    const location = req.body.location;
-    const roomCount = req.body.roomCount;
-    const ownerName = req.body.ownerName;
-    const ownerPhone = req.body.ownerPhone;
-    const roommates = req.body.roommates;
-    const remaining = req.body.Remaining;
-    const image = req.file.buffer.toString('base64');
-    db.query('INSERT INTO apartment SET ?', { monthlyCost: cost, space: space, description: description, city: city, location: location, roomCount: roomCount, ownerName: ownerName, ownerPhone: ownerPhone, imageHere: image, roommates: roommates, remainingRoommates: remaining }, (error, results) => {
-        if (error) {
-            console.log(error);
-        } else {
-            return res.redirect('/admin');
-        }
-
-    })
-})
 
 router.get('/info/:id', function(req, res, next) {
     console.log(req.param.id)
@@ -86,7 +55,7 @@ router.get('/info/:id', function(req, res, next) {
     })
 })
 
-router.get('/delete/:id', function(req, res, next) {
+router.delete('/delete/:id', function(req, res, next) {
     console.log("delete")
     let id = req.params.id;
     db.query('DELETE FROM apartment WHERE id = ? ', id, (error, rows) => {
@@ -97,4 +66,25 @@ router.get('/delete/:id', function(req, res, next) {
     })
 })
 
+router.get('/edit/:id', function(req, res, next) {
+  let id = req.params.id;
+  db.query('SELECT * FROM apartment WHERE id = ? ' ,[id], (error , rows ) =>{
+    if(error)console.log(error)
+    else{
+        res.render('edit',{rows});
+    }
+ })
+ })
+ 
+ router.put('/edit/:id', function(req, res, next) {
+   db.query('UPDATE apartment SET space = ?,monthlyCost = ?,description = ?,city = ?,location =?,roomCount = ?,ownerName=?,ownerPhone=?,roommates=?,remainingRoommates=? WHERE id = ?' , [req.body.space,req.body.monthlyCost,req.body.description,req.body.city,req.body.location,req.body.roomCount,req.body.ownerName,req.body.ownerPhone,req.body.roommates,req.body.Remaining, req.params.id], (error , rows ) =>{
+     if(error)console.log(error)
+     else{
+         res.redirect('/adminApartment');
+     }
+  
+ })
+ 
+    
+  })
 module.exports = router;
