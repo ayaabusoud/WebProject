fa37fee3387238234c302a657f011699ee6c67be
Sara Oraib <91946939+saraoraib@users.noreply.github.com>
2022-04-29 12:07:58 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -80,5 +80,30 @@ const router = express.Router();
       })
       });
       
-
+router.get('/edit/:id', function(req, res, next) {
+        let id = req.params.id;
+        db.query('SELECT * FROM user WHERE id = ? ' ,[id], (error , rows ) =>{
+          if(error)console.log(error)
+          else{
+              res.render('editProfile',{rows});
+          }
+       })
+       })
+       
+       router.post('/edit/:id', function(req, res, next) {
+         db.query('UPDATE user SET username = ?,phonenumber = ?,gender = ?,university = ? WHERE id = ?' , [req.body.username,req.body.phonenumber,req.body.gender,req.body.university, req.params.id], (error , rows ) =>{
+           if(error)console.log(error)
+           else{
+            db.query('SELECT * FROM user WHERE id = ? ' ,[req.params.id], (error , rows ) =>{
+              if(error)console.log(error)
+              else{
+                  res.render('profile',{rows});
+              }
+           })
+           }
+        
+       })
+       
+          
+        })
    module.exports = router ;
